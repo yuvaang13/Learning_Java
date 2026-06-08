b2d6a267aa96b2038b8a64c15a193bddd748e444
yuvaang13 <yuvaangulati7689@gmail.com>
2026-06-08 11:41:53 -0400

    initial commit

---
+++ src/variables_reference.java
@@ -0,0 +1,10 @@
+public class variables_reference {
+
+    public static void main(String[] args){
+
+        String name = "yuvaan";
+        System.out.println(name + " is cool");
+        String food = "sushi";//strings are the same here as in python, except u have to declare the data type instead of like doing name  = yuvaan
+        System.out.println("i think " + food + " is good (subjective af)");
+    }
+}
