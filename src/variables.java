a312d3664a616392590eea7a37ae7eab3f018e3a
yuvaang13 <yuvaangulati7689@gmail.com>
2026-06-08 11:19:40 -0400

    initial commit

---
+++ src/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+
+    public static void main(String[] args){
+
+        //i already know variables in python, lets see how different these are
+        int age = 21; //creating variable (declaring), then assigning (21)
+        System.out.println(age);//prints age
+        double year = 2026.45;//double is like a float (decimal)
+        System.out.println(year);//prints it
+    }
+}
