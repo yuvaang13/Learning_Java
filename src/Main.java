0f03ccb7740e995b099b91982bd7d8fd163845ca
yuvaang13 <yuvaangulati7689@gmail.com>
2026-06-08 11:04:59 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+
+//FIRST JAVA PROGRAM
+
+
 public class Main {
 
     public static void main(String[] args){//magic spell to get program running
 
-    }
+    System.out.print("hello world\n");          //semicolon acts as period, \n is better (like python)
+    System.out.print("hi, im new to Java\n");   //ln separates them so they go on different lines
+    System.out.print("i hope i get good at this quick\n");
+    System.out.print("sout and then tab autocompletes this System.out.print");
+    }                                            //this is way more unnecessarily complicated than python
 }
