698cc83a34d1374ac6dcde21c50fba3264a62803
yuvaang13 <yuvaangulati7689@gmail.com>
2026-06-09 15:10:30 -0400

    initial commit

---
+++ src/arithmetic_operators.java
@@ -0,0 +1,23 @@
+public class arithmetic_operators {
+
+    public static void main(String[] args){
+
+        int x = 6;
+        int y = 5;
+        int z;
+        z = x*y;//this is exactly like python
+        System.out.println(z);//except for the prints
+        z = x + y;
+        System.out.println(z);
+        z = x/y;//this gives an integer because when dividing to ints you get an int
+        System.out.println(z);
+        z = x-y;
+        System.out.println(z);
+
+        double a = 6;
+        double b = 5;
+        double c;
+        c = a/b;
+        System.out.println(c);//there we go
+    }
+}
