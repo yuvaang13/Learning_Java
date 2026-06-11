1f4d766f918acc8b73618316face918522e98d00
yuvaang13 <yuvaangulati7689@gmail.com>
2026-06-11 12:12:48 -0400

    initial commit

---
+++ src/math_methods.java
@@ -0,0 +1,28 @@
+public class math_methods {
+    public static void main(String[] args){
+
+        System.out.println(Math.PI);//prints π (if u dont know how to do this its option p on mac(idk windows)
+        System.out.println(Math.E);//euler's number
+
+        double ans;
+        ans = Math.pow(2,4); // exponent function equals 2^4
+        System.out.println(ans);
+        double answer;
+        answer = Math.abs(-15);// absolute value of -15 |-15|
+        System.out.println(answer);
+        answer = Math.sqrt(16);//square root of 16
+        System.out.println(answer);
+        answer = Math.round(6.676767676767);//rounds to the nearest int
+        System.out.println(answer);
+        answer = Math.ceil(3.14);//rounds up
+        System.out.println(answer);
+        answer = Math.floor(6.78789);//rounds to floor (down)
+        System.out.println(answer);
+        answer = Math.max(14,35345);//answer is assigned the largest number of the two
+        System.out.println(answer);
+        answer = Math.min(14,2354);//finds minimum of two numbers (least)
+        System.out.println(answer);
+
+
+    }
+}
