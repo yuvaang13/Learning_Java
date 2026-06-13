41140a3375cdca1d6b525101bb797a739d2ba799
yuvaang13 <yuvaangulati7689@gmail.com>
2026-06-12 23:04:02 -0400

    initial commit

---
+++ src/substrings.java
@@ -0,0 +1,8 @@
+public class substrings {
+    public static void main(String[] args){
+
+        String email = "yuvaangulati7689@gmail.com"; //reach out, lets change the world together
+        System.out.println(email.substring(0,7));//prints yuvaang
+
+    }
+}
